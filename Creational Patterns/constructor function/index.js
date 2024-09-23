2b7166852b27d5f1d1bfd7eedd2cce9df24fb639
LeshchuDev <leshchudev@gmail.com>
2024-09-23 13:59:50 +0200

    basic contructor function

--- Creational Patterns/constructor function/index.js
+++ Creational Patterns/constructor function/index.js
@@ -1,10 +1,13 @@
 function Product(brand, name, productionDate, price) {
   this.brand = brand;
   this.name = name;
-  this.productionDate - productionDate;
+  this.productionDate = productionDate;
   this.price = price;
 
   this.printInfo = () => {
     console.log(this.brand, this.name, this.productionDate, this.price);
   };
 }
+
+const computer = new Product('Apple', 'MacBook Pro 15', 2015, 8999)
+computer.printInfo()
