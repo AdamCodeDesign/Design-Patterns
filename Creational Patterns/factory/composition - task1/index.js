c273d371ef05c6f084eef8a2223f9f38645b28c3
LeshchuDev <leshchudev@gmail.com>
2024-09-28 14:19:51 +0200

    composition task 1 complete

---
+++ Creational Patterns/factory/composition - task1/index.js
@@ -0,0 +1,34 @@
+let programer = function (state) {
+  return {
+    useLanguage: function (language) {
+      state.language = language;
+    },
+
+    code: function (task) {
+      console.log(state.name, "is coding in language: ", state.language, task);
+    },
+  };
+};
+
+let tester = function (state) {
+  return {
+    test: function (program) {
+      console.log(state.name, "is testing: ", program);
+    },
+  };
+};
+
+let employee = function (name, surname) {
+  let state = {
+    name: name,
+    surname: surname,
+  };
+
+  return Object.assign(state, programer(state), tester(state));
+};
+
+let employee1 = employee("Adam", "Leszczyk");
+employee1.useLanguage("JavaScript");
+employee1.code("Awesome app");
+employee1.test('Awesome app')
+console.log(employee1)
